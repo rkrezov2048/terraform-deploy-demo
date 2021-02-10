10ba20baf033d05618d08f891b01a20aa058ac6c
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 12:52:30 +0100

    refactoring module

--- deploy.tf
+++ deploy.tf
@@ -1,19 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "demo"
-  region  = "us-east-1"
-}
+# -- root/main.tf ---
 
 module "vpc_demo_1" {
   source = "github.com/rkrezov2048/terraform-module-demo/demo-module"
+  cidr_block = "172.16.0.0/16"
+  public_cidrs = ["172.16.10.0/24", "172.16.20.0/24"]
+  
 }
 
 
---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "demo"
+  region  = var.aws_region
+}
