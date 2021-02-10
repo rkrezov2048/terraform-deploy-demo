0ddba00bb20257e4bd9d0398ee8fc5ede1659368
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 13:13:49 +0100

    added private subnets

--- deploy.tf
+++ deploy.tf
@@ -4,6 +4,7 @@ module "vpc_demo_1" {
   source = "github.com/rkrezov2048/terraform-module-demo/demo-module"
   cidr_block = "172.16.0.0/16"
   public_cidrs = ["172.16.10.0/24", "172.16.20.0/24"]
+  private_cidrs = ["172.16.100.0/24", "172.16.101.0/24", "172.16.102.0/24"]
   
 }
 
