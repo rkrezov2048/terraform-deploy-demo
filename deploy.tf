7301b4a575a01384fda0688e726d096bf5e819fd
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 15:42:40 +0100

    added provider

--- deploy.tf
+++ deploy.tf
@@ -1,11 +1,14 @@
 # -- root/main.tf ---
+locals {
+  vpc_cidr = "172.16.0.0/16"
+}
 
 module "vpc_demo_1" {
-  source = "github.com/rkrezov2048/terraform-module-demo/demo-module"
-  cidr_block = "172.16.0.0/16"
-  public_cidrs = ["172.16.10.0/24", "172.16.20.0/24"]
+  source        = "github.com/rkrezov2048/terraform-module-demo/demo-module"
+  cidr_block    = local.vpc_cidr
+  public_cidrs  = [for i in range(1, 3, 1) : cidrsubnet(local.vpc_cidr, 8, i)]
   private_cidrs = ["172.16.100.0/24", "172.16.101.0/24", "172.16.102.0/24"]
-  
+
 }
 
 
