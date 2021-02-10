c3cbb823f7fead4fbf9c605f5c3798bb5e51f65d
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 10:54:56 +0100

    initial files added

---
+++ deploy.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "demo"
+  region = "us-east-1"
+}
+
+module "vpc_demo_1" {
+  source = "github.com/rkrezov2048/terraform-module-demo/demo-module"
+}
