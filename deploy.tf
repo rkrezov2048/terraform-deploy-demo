f9557cb9cd480c4a325c9324cdb48aa3a6ec4728
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 12:17:32 +0100

    added an output

--- deploy.tf
+++ deploy.tf
@@ -1,17 +1,22 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 2.70"
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
     }
   }
 }
 
 provider "aws" {
   profile = "demo"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 module "vpc_demo_1" {
   source = "github.com/rkrezov2048/terraform-module-demo/demo-module"
+}
+
+
+output "vpc_id" {
+  value = module.vpc_demo_1.vpc_id
 }
