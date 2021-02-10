904f0f248b55ce35df3a3f47c9455861d75e9831
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 16:04:21 +0100

    added new variable

--- deploy.tf
+++ deploy.tf
@@ -6,6 +6,7 @@ locals {
 module "vpc_demo_1" {
   source        = "github.com/rkrezov2048/terraform-module-demo/demo-module"
   cidr_block    = local.vpc_cidr
+  access_ip     = var.access_ip
   public_cidrs  = [for i in range(1, 3, 1) : cidrsubnet(local.vpc_cidr, 8, i)]
   private_cidrs = ["172.16.100.0/24", "172.16.101.0/24", "172.16.102.0/24"]
 
