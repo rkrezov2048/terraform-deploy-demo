b7f082c5d4dca58e191c07316e27691d6e90dbf6
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 10:23:52 +0100

    added another sercurity group

--- locals.tf
+++ locals.tf
@@ -14,6 +14,24 @@ locals {
           protocol    = "tcp"
           cidr_blocks = [var.access_ip]
         }
+        http = {
+          from        = 80
+          to          = 80
+          protocol    = "tcp"
+          cidr_blocks = [var.access_ip]
+        }
+      }
+    }
+    rds = {
+      name        = "rds_sg"
+      description = "rds Access"
+      ingress = {
+        mysql = {
+          from        = 3306
+          to          = 3306
+          protocol    = "tcp"
+          cidr_blocks = [local.vpc_cidr]
+        }
       }
     }
   }
