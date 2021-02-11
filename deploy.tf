8b110bbce0bb305191f12a107a6a3d3369f99037
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 12:28:05 +0100

    added new outputs

--- deploy.tf
+++ deploy.tf
@@ -7,6 +7,21 @@ module "vpc_demo_1" {
   security_groups = local.security_groups
   public_cidrs    = [for i in range(1, 3, 1) : cidrsubnet(local.vpc_cidr, 8, i)]
   private_cidrs   = ["172.16.100.0/24", "172.16.101.0/24", "172.16.102.0/24"]
+  db_subnet_group = true
 
 }
 
+module "database_rds" {
+  source                 = ""
+  db_storage             = 10
+  db_instance_class      = "db.t2.micro"
+  db_engine_version      = "5.7.22"
+  db_name                = "rancher"
+  db_username            = "john"
+  db_password            = "tempus435"
+  vpc_security_group_ids = module.vpc_demo_1.security_rds
+  db_identifier          = "mtc-db"
+  db_subnet_group_name   = module.vpc_demo_1.subnet_group[0]
+  skip_final_snapshot    = true
+
+}
--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,13 @@ output "public_sub" {
 }
 
 output "private_sub" {
-  value = module.vpc_demo_1.public_sub
+  value = module.vpc_demo_1.private_sub
+}
+
+output "subnet_group" {
+  value = module.vpc_demo_1.db_subnet_group
+}
+
+output "security_rds" {
+  value = module.vpc_demo_1.db_sg
 }
